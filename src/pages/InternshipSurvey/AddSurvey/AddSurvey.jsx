3465eae4749012938d11388bf779d3468911ac9e
Billy Bett <billybett@brian-mac.local>
2022-11-28 16:21:38 +0300

    create internshipSurvey form view - added both survey inputs - added relevant route in the index.js file

---
+++ src/pages/InternshipSurvey/AddSurvey/AddSurvey.jsx
@@ -0,0 +1,23 @@
+import { Col, Row } from "antd";
+
+import { ReactComponent as AddItem } from "assets/additem.svg";
+import { ReactComponent as Menu } from "assets/menu.svg";
+
+import "./AddSurvey.scss";
+
+const AddField = () => {
+  return (
+    <Row justify="center" align="middle" className="addChapter">
+      <Col span={1}>
+        <Menu style={{ display: "none" }} />
+      </Col>
+      <Col span={23} className="addChapter__item">
+        <Row justify="center" align="middle" className="addChapter__item__button">
+          <AddItem style={{ marginRight: "10px" }} /> Add Chapter
+        </Row>
+      </Col>
+    </Row>
+  );
+};
+
+export default AddField;
