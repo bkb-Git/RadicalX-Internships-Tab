a7b647b99aca96e6afa3b8d5953f115caacaf418
Billy Bett <billybett@brian-mac.local>
2022-12-12 15:19:32 +0300

    rearrange: reworked folder structure of the pages folder

--- src/index.jsx
+++ src/index.jsx
@@ -2,18 +2,24 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
+// Main view components  //
+
 import MainLayout from "layout/MainLayout";
 import Internships from "pages/Internships";
 import PageNotFound from "pages/PageNotFound";
-import InternshipFormLayout from "layout/InternshipFormLayout";
-import InternshipDescription from "pages/InternshipDescription";
-import InternshipGuide from "pages/InternshipGuide";
-import InternshipSurvey from "pages/InternshipSurvey";
-import InternshipSettings from "pages/InternshipSettings";
-import InternshipDescriptionInput from "pages/InternshipDescription/InternshipDescriptionInput";
-import InternshipGuideInput from "pages/InternshipGuide/InternshipGuideInput";
-import InternshipSurveyInput from "pages/InternshipSurvey/InternshipSurveyInput";
-import InternshipSettingsInput from "pages/InternshipSettings/InternshipSettingsInput";
+
+// Add New Internship components //
+
+import AddNewInternship from "pages/AddNewInternship";
+
+import InternshipDescription from "pages/AddNewInternship/InternshipDescription";
+import InternshipGuide from "pages/AddNewInternship/InternshipGuide";
+import InternshipSurvey from "pages/AddNewInternship/InternshipSurvey";
+import InternshipSettings from "pages/AddNewInternship/InternshipSettings";
+import InternshipDescriptionInput from "pages/AddNewInternship/InternshipDescription/InternshipDescriptionInput";
+import InternshipGuideInput from "pages/AddNewInternship/InternshipGuide/InternshipGuideInput";
+import InternshipSurveyInput from "pages/AddNewInternship/InternshipSurvey/InternshipSurveyInput";
+import InternshipSettingsInput from "pages/AddNewInternship/InternshipSettings/InternshipSettingsInput";
 
 import "./styles/index.scss";
 
@@ -22,38 +28,45 @@ import "./styles/index.scss";
 const container = document.getElementById("root");
 const root = createRoot(container);
 
+const App = () => {
+  return (
+    <Routes>
+      {/* Main View is routed here */}
+
+      <Route path="/" element={<MainLayout />}>
+        <Route index element={<PageNotFound />} />
+        <Route path="internships" element={<Internships />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Route>
+
+      {/* Add New Internship view routed here */}
+
+      <Route path="internships/add" element={<AddNewInternship />}>
+        <Route path="description" element={<InternshipDescription />}>
+          <Route path=":fieldId" element={<InternshipDescriptionInput />} />
+        </Route>
+        <Route path="guide" element={<InternshipGuide />}>
+          <Route path=":fieldId" element={<InternshipGuideInput />} />
+        </Route>
+        <Route path="survey" element={<InternshipSurvey />}>
+          <Route path=":fieldId" element={<InternshipSurveyInput />} />
+        </Route>
+        <Route path="settings" element={<InternshipSettings />}>
+          <Route path=":fieldId" element={<InternshipSettingsInput />} />
+        </Route>
+      </Route>
+
+      {/* Page 404 route define here */}
+
+      <Route path="*" element={<PageNotFound />} />
+    </Routes>
+  );
+};
+
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <Routes>
-        {/* Main View is routed here */}
-
-        <Route path="/" element={<MainLayout />}>
-          <Route index element={<PageNotFound />} />
-          <Route path="internships" element={<Internships />} />
-          <Route path="*" element={<PageNotFound />} />
-        </Route>
-
-        {/* Add New Internship view routed here */}
-
-        <Route path="internships/add" element={<InternshipFormLayout />}>
-          <Route path="description" element={<InternshipDescription />}>
-            <Route path=":fieldId" element={<InternshipDescriptionInput />} />
-          </Route>
-          <Route path="guide" element={<InternshipGuide />}>
-            <Route path=":fieldId" element={<InternshipGuideInput />} />
-          </Route>
-          <Route path="survey" element={<InternshipSurvey />}>
-            <Route path=":fieldId" element={<InternshipSurveyInput />} />
-          </Route>
-          <Route path="settings" element={<InternshipSettings />}>
-            <Route path=":fieldId" element={<InternshipSettingsInput />} />
-          </Route>
-        </Route>
-
-        {/* Page 404 route define here */}
-        <Route path="*" element={<PageNotFound />} />
-      </Routes>
+      <App />
     </BrowserRouter>
   </React.StrictMode>
 );
--- src/layout/InternshipFormLayout/InternshipFormLayout.jsx
+++ src/layout/InternshipFormLayout/InternshipFormLayout.jsx
@@ -1,75 +1,25 @@
-import { useEffect, useState } from "react";
-import { Outlet, useLocation, useNavigate } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 
-import { updateDoc } from "firebase/firestore";
-
-import { Button, Form, Layout, Modal, notification, Result } from "antd";
-
-import { AddNewInternshipFormProvider } from "context/AddNewInternshipFormContext";
+import { Button, Form, Layout, Modal, Result } from "antd";
 
 import FormHeader from "./FormHeader";
 
 import "./InternshipFormLayout.scss";
 
-const ROUTES = {
-  0: "description",
-  1: "guide",
-  2: "survey",
-  3: "settings",
-};
-
 const { Content } = Layout;
 
-const InternshipFormLayout = () => {
-  // Form state defined here
+const InternshipFormLayout = (props) => {
+  const { isModalOpen, setIsModalOpen, contextHolder } = props;
 
-  const [step, setStep] = useState(0);
-  const [isModalOpen, setIsModalOpen] = useState(false);
-
-  // Notification API //
-
-  const [api, contextHolder] = notification.useNotification();
-
-  // React-router-dom hook functions //
+  // Navigate hook defined here //
 
   const navigate = useNavigate();
-  const location = useLocation();
 
   // Handler functions //
 
   const handleCancel = () => setIsModalOpen(false);
 
   const handleOk = () => navigate("/internships", { replace: true });
-
-  const handleFormFinish = (docRef, setSubmitting) => {
-    updateDoc(docRef, "formFinished", true)
-      .then(
-        () => {
-          setSubmitting(false);
-          setIsModalOpen(true);
-        },
-        () =>
-          api.error({
-            message: "Failed to Submit Internship",
-            placement: "bottomLeft",
-          })
-      )
-      .catch((err) =>
-        api.error({
-          message: "Failed to Submit Document",
-          description: err.message,
-          placement: "bottomLeft",
-        })
-      );
-  };
-
-  // React life-cycle method that handles current step view //
-
-  useEffect(() => {
-    const currentPath = location.pathname;
-
-    if (!currentPath.match(ROUTES[step])) navigate(ROUTES[step]);
-  }, [step, location.pathname]);
 
   // Render functions for views //
 
@@ -94,18 +44,16 @@ const InternshipFormLayout = () => {
   };
 
   return (
-    <AddNewInternshipFormProvider otherValues={{ step, setStep, handleFormFinish }}>
-      <Layout className="internshipsLayout">
-        <FormHeader />
-        <Content className="internshipsLayout__content">
-          <Form name="newInternship" className="internshipsLayout__content__newInternship">
-            <Outlet />
-          </Form>
-        </Content>
-      </Layout>
+    <Layout className="internshipsLayout">
+      <FormHeader />
+      <Content className="internshipsLayout__content">
+        <Form name="newInternship" className="internshipsLayout__content__newInternship">
+          <Outlet />
+        </Form>
+      </Content>
       {renderModal()}
       {contextHolder}
-    </AddNewInternshipFormProvider>
+    </Layout>
   );
 };
 
