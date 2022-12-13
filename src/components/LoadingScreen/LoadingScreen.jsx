52efaa75045697152a343c13841e4e462132d86e
Billy Bett <billybett@brian-mac.local>
2022-12-13 21:26:51 +0300

    fix: fixed size of loader icon on the loading screen

--- src/components/LoadingScreen/LoadingScreen.jsx
+++ src/components/LoadingScreen/LoadingScreen.jsx
@@ -3,7 +3,7 @@ import { Row, Spin } from "antd";
 const LoadingScreen = () => {
   return (
     <Row justify="center" align="middle" style={{ height: "100vh" }}>
-      <Spin spinning />
+      <Spin size="large" spinning />
     </Row>
   );
 };
